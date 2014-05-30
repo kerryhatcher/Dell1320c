04317f6dcce3993b275a426a151a57b4f8926325
Kerry Hatcher <kwhatcher@gmail.com>
2014-05-30 19:06:03 -0400

    It works!!!!

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp -r ./opt/Dell1320 /opt/Dell1320
+
+mkdir /usr/share/cups/model/Dell
+
+mkdir /usr/share/cups/model/Dell/en
+
+cp ./Dell1320c_32bit.ppd /usr/share/cups/model/Dell/en/Dell1320c_32bit.ppd
